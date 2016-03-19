84b65ac6e68f0d81746d2c33d758c13f7d68905e
fanwei <fcthao@icloud.com>
2016-03-20 01:29:27 +0800

    将导航控制器前嵌入TarBarController

---
+++ 换肤/ThreeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ThreeViewController.swift
+//  换肤
+//
+//  Created by 范伟 on 16/3/20.
+//  Copyright © 2016年 FanWei. All rights reserved.
+//
+
+import UIKit
+
+class ThreeViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
